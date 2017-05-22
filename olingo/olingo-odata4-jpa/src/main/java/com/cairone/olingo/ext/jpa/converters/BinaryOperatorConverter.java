741e3d777387fdd37e713fe357962210cdffa224
diegocairone <diegocairone@gmail.com>
2017-05-22 09:59:26 -0300

    Cleaning up - Converters

--- olingo/olingo-odata4-jpa/src/main/java/com/cairone/olingo/ext/jpa/converters/BinaryOperatorConverter.java
+++ olingo/olingo-odata4-jpa/src/main/java/com/cairone/olingo/ext/jpa/converters/BinaryOperatorConverter.java
@@ -1,9 +1,27 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.cairone.olingo.ext.jpa.converters;
 
 import org.apache.olingo.server.api.uri.queryoption.expression.BinaryOperatorKind;
 
 import com.cairone.olingo.ext.jpa.interfaces.OperatorConverter;
 
+/**
+ * String representation of <code>org.apache.olingo.server.api.uri.queryoption.expression.BinaryOperatorKind</code> for use in JPQL
+ * 
+ * @author diego.cairone
+ */
 public class BinaryOperatorConverter implements OperatorConverter<BinaryOperatorKind, String> {
 
 	@Override
