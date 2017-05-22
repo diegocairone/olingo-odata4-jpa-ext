31d789bf3211e387254cd784fde3f31d93ed1319
diegocairone <diegocairone@gmail.com>
2017-05-22 10:03:27 -0300

    Cleaning up - Enums

--- olingo/olingo-odata4-jpa/src/main/java/com/cairone/olingo/ext/jpa/enums/BinaryOperatorGroup.java
+++ olingo/olingo-odata4-jpa/src/main/java/com/cairone/olingo/ext/jpa/enums/BinaryOperatorGroup.java
@@ -1,7 +1,25 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.cairone.olingo.ext.jpa.enums;
 
 import org.apache.olingo.server.api.uri.queryoption.expression.BinaryOperatorKind;
 
+/**
+ * Enumeration for grouping different kinds of binary operations
+ * 
+ * @author diego.cairone
+ */
 public enum BinaryOperatorGroup {
 	ARITHMETIC_OPERATOR, LOGICAL_OPERATOR, COMPARISON_OPERATOR, UNKNOWN;
 	
