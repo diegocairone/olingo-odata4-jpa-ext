78d5764865a5039317d9ad639ad890b143f63c6b
diegocairone <diegocairone@gmail.com>
2018-07-02 09:39:23 -0300

    Logging errors

--- olingo/olingo-odata4-jpa/src/main/java/com/cairone/olingo/ext/jpa/query/QuerydslQuery.java
+++ olingo/olingo-odata4-jpa/src/main/java/com/cairone/olingo/ext/jpa/query/QuerydslQuery.java
@@ -2,14 +2,23 @@ package com.cairone.olingo.ext.jpa.query;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
+import org.apache.olingo.commons.api.http.HttpStatusCode;
+import org.apache.olingo.server.api.ODataApplicationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import com.cairone.olingo.ext.jpa.processors.BaseProcessor;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 
 public class QuerydslQuery {
 
+	protected static final Logger LOG = LoggerFactory.getLogger(BaseProcessor.class);
+	
 	private BooleanExpression booleanExpression = null;
 	private OrderSpecifier<?>[] orderSpecifiers = null;
 	
@@ -34,10 +43,19 @@ public class QuerydslQuery {
 	}
 
 	@SuppressWarnings("unchecked")
-	public static <T> List<T> execute(QuerydslPredicateExecutor<?> queryDslPredicateExecutor, QuerydslQuery dslQuery) {
+	public static <T> List<T> execute(QuerydslPredicateExecutor<?> queryDslPredicateExecutor, QuerydslQuery dslQuery) throws ODataApplicationException {
 		
-		return dslQuery.getOrderSpecifiers() == null
-				? (List<T>) queryDslPredicateExecutor.findAll(dslQuery.getBooleanExpression())
-				: (List<T>) queryDslPredicateExecutor.findAll(dslQuery.getBooleanExpression(), dslQuery.getOrderSpecifiers());
+		try {
+			return dslQuery.getOrderSpecifiers() == null
+					? (List<T>) queryDslPredicateExecutor.findAll(dslQuery.getBooleanExpression())
+					: (List<T>) queryDslPredicateExecutor.findAll(dslQuery.getBooleanExpression(), dslQuery.getOrderSpecifiers());
+		} catch(DataAccessException e) {
+			LOG.error(e.getMessage(), e);
+			throw new ODataApplicationException(
+					e.getMessage(), 
+					HttpStatusCode.INTERNAL_SERVER_ERROR.getStatusCode(), 
+					Locale.ENGLISH,
+					e);
+		}
 	}
 }
